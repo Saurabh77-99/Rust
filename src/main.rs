9846743d6d345b2741f932490733cba6ec486f13
Saurabh Davda <saurabhdavda9977@gmail.com>
2024-05-04 19:59:29 +0530

    Rust program runs!!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let x:i32 = -5;
+    let y:u32 = 500;
+    let z:f32 = 500.05;
+
+    println!("x:{},y:{},z:{}",x,y,z);
+    println!("x:{}",x);
+    println!("y:{}",y);
+    println!("z:{}",z);
 }
